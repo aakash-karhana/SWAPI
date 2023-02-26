d2bcc9b465fcff2e70bf8359b783ab13c3c93346
Aakash karhana <aakashkarhana@gmail.com>
2023-02-26 10:59:13 +0530

    resources module

---
+++ resources/base.py
@@ -0,0 +1,25 @@
+"""
+This module specially dedicated for resource base classes where we have to implement all the functions defined under
+this module
+"""
+
+
+class ResourceBase(object):
+    """
+    Base class representing required methods to be implemented by all resource classes
+    """
+
+    resources = ["people", "planets", "starships",
+                 "vehicles", "films", "species"]
+
+    def __init__(self):
+        self.home_url = "https://swapi.dev/"
+
+    def get_count(self):
+        raise NotImplementedError
+
+    def get_resource_urls(self):
+        raise NotImplementedError
+
+    def get_sample_data(self):
+        raise NotImplementedError
